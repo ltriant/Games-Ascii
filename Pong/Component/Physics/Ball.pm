57ae3857d0c4f77cf7484df112013c607f1ab5e0
Luke Triantafyllidis <ltriant@cpan.org>
2015-09-02 12:09:29 +1000

    Calculate ball physics based on where it's going to be next, not where it is now

--- Pong/Component/Physics/Ball.pm
+++ Pong/Component/Physics/Ball.pm
@@ -10,6 +10,9 @@ sub update {
 
 	my ($x, $y) = @{ $ball->position };
 	my ($ew, $ns) = @{ $ball->direction };
+	my $v = $ball->velocity;
+
+	my ($nx, $ny) = ($x + $v, $y + $v);
 
 	foreach my $obj ($game->objects) {
 		next if $obj eq $ball;
@@ -17,10 +20,10 @@ sub update {
 		my ($ox, $oy) = @{ $obj->position };
 		my ($sz_x, $sz_y) = @{ $obj->size };
 
-		if (    ($x >= $ox)
-			and ($x <= $ox + $sz_x)
-			and ($y >= $oy)
-			and ($y <= $oy + $sz_y)
+		if (    ($nx >= $ox)
+			and ($nx <= $ox + $sz_x)
+			and ($ny >= $oy)
+			and ($ny <= $oy + $sz_y)
 		) {
 			printf "  Hitting a %s object!\n", ref $obj;
 			printf "    ox, oy: %d, %d\n", $ox, $oy;
@@ -33,13 +36,6 @@ sub update {
 			elsif ($ns eq 'S') {
 				$ns = $ball->direction->[1] = 'N';
 			}
-
-#			if ($ew eq 'W') {
-#				$ew = $ball->direction->[0] = 'E';
-#			}
-#			elsif ($ew eq 'E') {
-#				$ew = $ball->direction->[0] = 'W';
-#			}
 
 			printf "    New direction: %s%s\n", $ns, $ew;
 		}
@@ -60,10 +56,10 @@ sub update {
 		$ew = $ball->direction->[0] = 'W';
 	}
 
-	$y = $ball->position->[1] += $ball->velocity if $ns eq 'N';
-	$y = $ball->position->[1] -= $ball->velocity if $ns eq 'S';
-	$x = $ball->position->[0] += $ball->velocity if $ew eq 'E';
-	$x = $ball->position->[0] -= $ball->velocity if $ew eq 'W';
+	$y = $ball->position->[1] += $v if $ns eq 'N';
+	$y = $ball->position->[1] -= $v if $ns eq 'S';
+	$x = $ball->position->[0] += $v if $ew eq 'E';
+	$x = $ball->position->[0] -= $v if $ew eq 'W';
 }
 
 1;
