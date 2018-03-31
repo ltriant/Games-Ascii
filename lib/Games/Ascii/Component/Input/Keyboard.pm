857d690f8f12db0986514e3ead03400055006664
Luke Triantafyllidis <ltriant@cpan.org>
2018-03-31 18:51:14 +1100

    Fix stupid bugs for velocity in the Y direction

--- lib/Games/Ascii/Component/Input/Keyboard.pm
+++ lib/Games/Ascii/Component/Input/Keyboard.pm
@@ -4,26 +4,26 @@ use Moo;
 extends 'Games::Ascii::Component::Input';
 
 sub update {
-	my ($self, $game, $paddle, $key) = @_;
+	my ($self, $game, $object, $key) = @_;
 
-	#printf "    %s got input: %s\n", ref($paddle), $key;
+	#printf "    %s got input: %s\n", ref($object), $key;
 
-	my ($vx, $vy) = @{ $paddle->velocity };
+	my ($vx, $vy) = @{ $object->velocity };
 
 	if ($self->left and (uc $key eq $self->left)) {
-		$paddle->velocity->[0] = $vx - 1;
+		$object->velocity->[0] = $vx - 1;
 	}
 
 	if ($self->right and (uc $key eq $self->right)) {
-		$paddle->velocity->[0] = $vx + 1;
+		$object->velocity->[0] = $vx + 1;
 	}
 
 	if ($self->up and (uc $key eq $self->up)) {
-		$paddle->velocity->[1] = $vy + 1;
+		$object->velocity->[1] = $vy - 1;
 	}
 
 	if ($self->down and (uc $key eq $self->down)) {
-		$paddle->velocity->[1] = $vy - 1;
+		$object->velocity->[1] = $vy + 1;
 	}
 }
 
